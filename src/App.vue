9c3e381b38df4599ae7bb27a4ef8cee6aa5a2473
BiancaSeleveschi <bianca.seleveschi@gmail.com>
2022-12-06 15:07:17 +0200

    add first project files  - install bootstrap library  - add books list  - create component ItemList  - create showReadUnreadBooks, moveReadOrUnreadBooks, deleteBook methods in ItemList  - create props  - import ItemList in App.vue for Books, unread books and read books  - add computed and filter functionalities  - create show and move to read/unread functionalities  - add showBook for diplay or not the books when you click on  - create deleteBook methods

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <ItemList @delete-book="deleteBook" :show-books="showBook" @show-read-unread-books="showAllBooks"
+                name="Books"
+                v-bind:class="[showBook ? 'bg-primary text-light' :'bg-secondary rounded-2  w-25 m-auto text-dark']"
+                :item-list="books"
+      />
+    </div>
+    <div class="container mt-5">
+      <div class="row d-flex justify-content-between">
+        <div class="col">
+          <ItemList @move-read-unread="moveUnreadBook" @show-read-unread-books="showUnreadBooks"
+                    :show-books="isUnreadBooks"
+                    name="Unread books"
+                    :item-list="unreadBooks"
+          />
+        </div>
+        <div class="col">
+          <ItemList @move-read-unread="moveReadBook" @show-read-unread-books="showReadBooks"
+                    :show-books="isReadBooks"
+                    name="Read books"
+                    :item-list="readBooks"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ItemList from "@/components/ItemList";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ItemList,
+  },
+  data() {
+    return {
+      books: [
+        {title: "Secretele succesului", authorName: "Dale Carnegie", releaseYear: 1936, read: true, readYear: ''},
+        {title: "Puterea intentiei", authorName: "Waine Dyer", releaseYear: 2015, read: true, readYear: ''},
+        {title: "Mindset", authorName: "Carol S. Dweck", releaseYear: 1987, read: false, readYear: ''},
+        {title: "Cand corpul spune nu", authorName: "Gabor Mate", releaseYear: 2018, read: false, readYear: ''},
+        {title: "Arta manipularii", authorName: "Kevin Dutton", releaseYear: 2019, read: false, readYear: ''},],
+      isReadBooks: true,
+      isUnreadBooks: true,
+      showBook: false
+    }
+  },
+  computed: {
+    unreadBooks() {
+      return this.books.filter(book => !book.read)
+    },
+    readBooks() {
+      return this.books.filter(book => book.read)
+    }
+  },
+  methods: {
+    moveReadBook(index) {
+      this.readBooks[index].read = !this.readBooks[index].read
+      this.readBooks.push(this.unreadBooks[index])
+      this.unreadBooks.splice(index, 1)
+    },
+    moveUnreadBook(index) {
+      this.unreadBooks[index].read = !this.unreadBooks[index].read
+      this.unreadBooks.push(this.readBooks[index])
+      this.readBooks.splice(index, 1)
+    },
+    deleteBook(index) {
+      this.books.splice(index, 1)
+    },
+    showReadBooks() {
+    this.isReadBooks = ! this.isReadBooks
+    },
+    showUnreadBooks() {
+    this.isUnreadBooks = !this.isUnreadBooks
+    },
+    showAllBooks() {
+      this.showBook = !this.showBook
+    }
   }
 }
 </script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1 @click="showReadUnreadBooks">{{ name }}</h1>
+    <ul v-show="showBooks" class="list-unstyled">
+      <li v-for="(book, index) in itemList" v-bind:key="index"
+          class="border rounded-4 bg-warning p-3 my-4 w-25 mx-auto">
+        <h2>{{ book.title }}</h2>
+        <h4>Author name: {{ book.authorName }}</h4>
+        <p>Release year: {{ book.releaseYear }}</p>
+        <button @click="moveReadOrUnread(index)"
+                class='mx-3'
+                v-bind:class="[book.read ? 'btn btn-success' : 'btn btn-primary']">
+          {{ book.read ? 'Unread' : 'Read' }}
+        </button>
+        <button @click="deleteBook" class="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemList",
+  props: ['itemList', 'name', 'showBooks'],
+  methods: {
+    showReadUnreadBooks() {
+      this.$emit('show-read-unread-books')
+    },
+    moveReadOrUnread(index) {
+      this.$emit('move-read-unread', index)
+    },
+    deleteBook(index) {
+      this.$emit('delete-book', index)
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
